c195a787cd5c183fd7502383f47984d9dd16fa3f
Pan1194162580 <1194162580@qq.com>
2020-03-25 10:24:27 +0800

    上传了 sql

--- README.md
+++ README.md
@@ -13,69 +13,73 @@
 
 由 **Spring Boot + Spring Security + MyBatis** 等技术实现的个人网站，在家没事做？不如跟我一起做网站吧，每天收入0元，我和身边朋友都在做，反正闲着也是闲着:dog:
 
-演示地址：http://www.pan176.com
+**演示地址：**http://www.pan176.com
 
-项目文档：http://www.pan176.top/webpersonal_doc
+**项目文档：**http://www.pan176.top/webpersonal_doc
 
-前端模板：[Vue Admin Template](https://github.com/PanJiaChen/vue-admin-template)
+**前端模板：**[Vue Admin Template](https://github.com/PanJiaChen/vue-admin-template)
+
+## 本地部署
+
+> 项目搭配阿里云 OSS使用，需要阿里云的 ACCESS_KEY_ID/ACCESS_KEY_SECRET
+
+1. **克隆项目到本地**
+
+   ```shell
+   git clone https://github.com/pan176/WebPersonal.git
+   ```
+
+2. **修改阿里云的相关配置**
+
+3. **数据库注入 webpersonal.sql**
+
+4. **本地部署 Nacos**
+
+   ```shell
+   git clone https://github.com/alibaba/nacos.git
+   cd nacos/
+   mvn -Prelease-nacos -Dmaven.test.skip=true clean install -U  
+   ls -al distribution/target/
+   
+   // 选择你下载的版本号替换$version
+   cd distribution/target/nacos-server-$version/nacos/bin
+   cmd startup.cmd
+   ```
+
+5. **启动 business、gateway**
+
+   ```shell
+   java -jar business-1.0.0-SNAPSHOT.jar
+   java -jar gateway-1.0.0-SNAPSHOT.jar
+   ```
+
+6. **启动 frontend**
+
+   ```shell
+   npm run dev
+   ```
+
+7. **访问 http://127.0.0.1:9527**
+
+## 技术支持
+
+**后端：**Spring Boot、Spring Cloud、Spring Security、Nacos、TkMybatis、HikariCP、OkHttp3、Jackson、Lombok、Docker
+
+**前端：**Vue、Axios、Element UI、Vue Element Admin
+
+**环境：**JDK 1.8、MySQL 8.0.17、Redis 5.0.6
 
 ## 项目结构
 
 ```
 WebPersonal
-├── business -- 业务服务
-├── gateway -- 网关服务
-└── frontend -- 前端界面
+    ├── business -- 业务服务
+    ├── gateway  -- 网关服务
+    └── frontend -- 前端界面
 ```
-
-## 后端技术
-
-| 技术                                                         | 说明                        |
-| ------------------------------------------------------------ | --------------------------- |
-| [Spring Boot](https://spring.io/projects/spring-boot)        | MVC框架                     |
-| [Spring Cloud Alibaba](https://spring.io/projects/spring-cloud-alibaba) | 微服务开发的一站式解决方案  |
-| [Spring Security oAuth2](https://spring.io/projects/spring-security-oauth) | 认证和授权框架              |
-| [TkMyBatis]()                                                | 基于 MyBatis 框架的一个工具 |
-| [MyBatisCodeHelperPro](https://www.baidu.com/link?url=8CK-ANR7tXoyIxsVK6gBR1SNF-kPeJY2gvBISlyztPPAxLERrrynNJJ_8wj73T6ZnouYVH9ekFLzZ97G7ANNNa&wd=&eqid=ef1fd0e2000cd5b4000000065de278fb) | 生成 MyBatis 相关代码       |
-| [OkHttp3](https://square.github.io/okhttp/)                  | 网络请求的框架              |
-| [Jackson](https://www.baidu.com/link?url=kTs9ZdJ-uFzbdHoqQNaEmr0rJrayLClHHSDJwoqrLnQoDtgME7NQMBfMb55EXdx6UAb35JVhCXFQezJPgToRMq&wd=&eqid=950f9c22002a4fe7000000065de2796b) | 提供了处理 JSON 数据的工具  |
-| [Lombok](	https://github.com/rzwitserloot/lombok)         | 简化对象封装工具            |
-| [Docker](https://www.docker.com/)                            | 应用容器引擎                |
-| [Redis](https://redis.io/)                                   | 分布式缓存                  |
-| [HikariCP](https://github.com/brettwooldridge/HikariCP)      | 数据库连接池                |
-| [Nacos](https://github.com/alibaba/nacos)                    | 注册中心                    |
-
-## 前端技术
-
-| 技术                                                         | 说明             |
-| ------------------------------------------------------------ | ---------------- |
-| [Vue]( https://vuejs.org/ )                                  | 前端框架         |
-| [Vue Router](https://router.vuejs.org/)                      | 路由框架         |
-| [Vuex](https://vuex.vuejs.org/)                              | 全局状态管理框架 |
-| [Axios](https://www.baidu.com/link?url=NKzjfqpbSMXR4AjHRO2EtLxVgP5dgLrWeC0zJl1Frug1TlHStw1CJVp9n4eAAdF0&wd=&eqid=ff4d26e200159260000000065de27b5d) | 前端 HTTP 框架   |
-| [Element UI](https://element.eleme.io/)                      | 前端 UI 框架     |
-| [Vue Element Admin](https://panjiachen.github.io/vue-element-admin-site/zh/) | 前端模板         |
-
-## 开发工具
-
-| 工具                                                       | 说明                 |
-| ---------------------------------------------------------- | -------------------- |
-| [IntelliJ IDEA](https://www.jetbrains.com/idea/download)   | 开发 IDE             |
-| [RedisDesktop](https://redisdesktop.com/download)          | Redis 客户端连接工具 |
-| [VMware](https://www.vmware.com/)                          | 虚拟机               |
-| [Xshell](http://www.netsarang.com/download/software.html)  | Linux 远程连接工具   |
-| [SQLyog](https://sqlyog.en.softonic.com/download)          | 数据库连接工具       |
-| [Postman](https://www.getpostman.com/downloads/)           | HTTP 请求工具        |
-| [亿图图示](https://www.edrawsoft.cn/download-edrawmax.php) | 流程图绘制工具       |
-
-## 开发环境
-
-| 工具                                                         | 版本号 |
-| ------------------------------------------------------------ | ------ |
-| [JDK](https://www.oracle.com/technetwork/java/javase/downloads/jdk8-downloads-2133151.html) | 1.8    |
-| [MySQL](https://www.mysql.com/)                              | 8.0.17 |
-| [Redis](https://redis.io/download)                           | 5.0.6  |
 
 ## 效果图
 
-![8qf7o6.jpg](https://s1.ax1x.com/2020/03/24/8qf7o6.jpg)
+![8XZnQP.jpg](https://s1.ax1x.com/2020/03/25/8XZnQP.jpg)
+
+![8XZ2y6.jpg](https://s1.ax1x.com/2020/03/25/8XZ2y6.jpg)
---
+++ sql/webpersonal.sql
@@ -0,0 +1,211 @@
+/*
+SQLyog Ultimate v12.5.0 (64 bit)
+MySQL - 8.0.13 : Database - webpersonal
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`webpersonal` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `webpersonal`;
+
+/*Table structure for table `clientdetails` */
+
+DROP TABLE IF EXISTS `clientdetails`;
+
+CREATE TABLE `clientdetails` (
+  `appId` varchar(128) NOT NULL,
+  `resourceIds` varchar(256) DEFAULT NULL,
+  `appSecret` varchar(256) DEFAULT NULL,
+  `scope` varchar(256) DEFAULT NULL,
+  `grantTypes` varchar(256) DEFAULT NULL,
+  `redirectUrl` varchar(256) DEFAULT NULL,
+  `authorities` varchar(256) DEFAULT NULL,
+  `access_token_validity` int(11) DEFAULT NULL,
+  `refresh_token_validity` int(11) DEFAULT NULL,
+  `additionalInformation` varchar(4096) DEFAULT NULL,
+  `autoApproveScopes` varchar(256) DEFAULT NULL,
+  PRIMARY KEY (`appId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Table structure for table `oauth_access_token` */
+
+DROP TABLE IF EXISTS `oauth_access_token`;
+
+CREATE TABLE `oauth_access_token` (
+  `token_id` varchar(256) DEFAULT NULL,
+  `token` blob,
+  `authentication_id` varchar(128) NOT NULL,
+  `user_name` varchar(256) DEFAULT NULL,
+  `client_id` varchar(256) DEFAULT NULL,
+  `authentication` blob,
+  `refresh_token` varchar(256) DEFAULT NULL,
+  PRIMARY KEY (`authentication_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Table structure for table `oauth_approvals` */
+
+DROP TABLE IF EXISTS `oauth_approvals`;
+
+CREATE TABLE `oauth_approvals` (
+  `userId` varchar(256) DEFAULT NULL,
+  `clientId` varchar(256) DEFAULT NULL,
+  `scope` varchar(256) DEFAULT NULL,
+  `status` varchar(10) DEFAULT NULL,
+  `expiresAt` timestamp NULL DEFAULT NULL,
+  `lastModifiedAt` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Table structure for table `oauth_client_details` */
+
+DROP TABLE IF EXISTS `oauth_client_details`;
+
+CREATE TABLE `oauth_client_details` (
+  `client_id` varchar(128) NOT NULL,
+  `resource_ids` varchar(256) DEFAULT NULL,
+  `client_secret` varchar(256) DEFAULT NULL,
+  `scope` varchar(256) DEFAULT NULL,
+  `authorized_grant_types` varchar(256) DEFAULT NULL,
+  `web_server_redirect_uri` varchar(256) DEFAULT NULL,
+  `authorities` varchar(256) DEFAULT NULL,
+  `access_token_validity` int(11) DEFAULT NULL,
+  `refresh_token_validity` int(11) DEFAULT NULL,
+  `additional_information` varchar(4096) DEFAULT NULL,
+  `autoapprove` varchar(256) DEFAULT NULL,
+  PRIMARY KEY (`client_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Table structure for table `oauth_client_token` */
+
+DROP TABLE IF EXISTS `oauth_client_token`;
+
+CREATE TABLE `oauth_client_token` (
+  `token_id` varchar(256) DEFAULT NULL,
+  `token` blob,
+  `authentication_id` varchar(128) NOT NULL,
+  `user_name` varchar(256) DEFAULT NULL,
+  `client_id` varchar(256) DEFAULT NULL,
+  PRIMARY KEY (`authentication_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Table structure for table `oauth_code` */
+
+DROP TABLE IF EXISTS `oauth_code`;
+
+CREATE TABLE `oauth_code` (
+  `code` varchar(256) DEFAULT NULL,
+  `authentication` blob
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Table structure for table `oauth_refresh_token` */
+
+DROP TABLE IF EXISTS `oauth_refresh_token`;
+
+CREATE TABLE `oauth_refresh_token` (
+  `token_id` varchar(256) DEFAULT NULL,
+  `token` blob,
+  `authentication` blob
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Table structure for table `tb_album` */
+
+DROP TABLE IF EXISTS `tb_album`;
+
+CREATE TABLE `tb_album` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `path` varchar(4500) NOT NULL,
+  `describe` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=utf8;
+
+/*Table structure for table `tb_permission` */
+
+DROP TABLE IF EXISTS `tb_permission`;
+
+CREATE TABLE `tb_permission` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `parent_id` bigint(20) DEFAULT NULL COMMENT '父权限',
+  `name` varchar(64) NOT NULL COMMENT '权限名称',
+  `enname` varchar(64) NOT NULL COMMENT '权限英文名称',
+  `url` varchar(255) NOT NULL COMMENT '授权路径',
+  `description` varchar(200) DEFAULT NULL COMMENT '备注',
+  `created` datetime NOT NULL,
+  `updated` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=38 DEFAULT CHARSET=utf8 COMMENT='权限表';
+
+/*Table structure for table `tb_resume` */
+
+DROP TABLE IF EXISTS `tb_resume`;
+
+CREATE TABLE `tb_resume` (
+  `id` bigint(20) DEFAULT NULL,
+  `content` text
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Table structure for table `tb_role` */
+
+DROP TABLE IF EXISTS `tb_role`;
+
+CREATE TABLE `tb_role` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `parent_id` bigint(20) DEFAULT NULL COMMENT '父角色',
+  `name` varchar(64) NOT NULL COMMENT '角色名称',
+  `enname` varchar(64) NOT NULL COMMENT '角色英文名称',
+  `description` varchar(200) DEFAULT NULL COMMENT '备注',
+  `created` datetime NOT NULL,
+  `updated` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=47 DEFAULT CHARSET=utf8 COMMENT='角色表';
+
+/*Table structure for table `tb_role_permission` */
+
+DROP TABLE IF EXISTS `tb_role_permission`;
+
+CREATE TABLE `tb_role_permission` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `role_id` bigint(20) NOT NULL COMMENT '角色 ID',
+  `permission_id` bigint(20) NOT NULL COMMENT '权限 ID',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=59 DEFAULT CHARSET=utf8 COMMENT='角色权限表';
+
+/*Table structure for table `tb_user` */
+
+DROP TABLE IF EXISTS `tb_user`;
+
+CREATE TABLE `tb_user` (
+  `id` bigint(20) NOT NULL COMMENT 'ID',
+  `username` varchar(576) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '用户名',
+  `password` varchar(576) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '密码',
+  `icon` varchar(4500) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '头像',
+  `email` varchar(4500) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '邮箱',
+  `nick_name` varchar(1800) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '昵称',
+  `note` varchar(4500) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '备注',
+  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+  `update_time` datetime DEFAULT NULL COMMENT '更新时间',
+  `status` int(1) DEFAULT NULL COMMENT '是否启用，1是启动，0是关闭',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Table structure for table `tb_user_role` */
+
+DROP TABLE IF EXISTS `tb_user_role`;
+
+CREATE TABLE `tb_user_role` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `user_id` bigint(20) NOT NULL COMMENT '用户 ID',
+  `role_id` bigint(20) NOT NULL COMMENT '角色 ID',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=37 DEFAULT CHARSET=utf8 COMMENT='用户角色表';
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
